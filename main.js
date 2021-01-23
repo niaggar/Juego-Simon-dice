c0450b9c99b8fd8637b3ddf37b8b857ed4a895c1
Nicolas Aguilera García <niagar25@gmail.com>
2021-01-23 14:10:38 -0500

    Implementación de la función que ilumina la secuencia de la partida

--- main.js
+++ main.js
@@ -7,32 +7,35 @@ const btnModalYes = document.getElementById( 'btn-ventana-yes' )
 const btnModalNo = document.getElementById( 'btn-ventana-no' )
 
 const modalRestart = document.getElementById( 'modal-restart' )
+const table = document.querySelector( '.tablero' )
 
 var newGame
+
+
+table.addEventListener( 'click', value => {
+  console.log( value )
+} )
 
 window.addEventListener( 'load',() => {
   btnRestart.disabled = true
 } )
 
 btnStart.addEventListener( 'click', () => {
-  newGame = new game()
   console.log( 'Start game' )
+  newGame = new game()
 } )
 
 
 btnRestart.addEventListener( 'click', () => {
   
   return new Promise( function( resolve, reject ){
-
     modalRestart.classList.toggle( 'ventana-cerrar' )
-    
     btnModalYes.addEventListener( 'click', () => {
       resolve( 'Restarting the game...' )
     } )
     btnModalNo.addEventListener( 'click', () => {
       reject( 'Cancel' )
     } )
-    
     
   } ).then( value => {
     console.log( value )
@@ -52,39 +55,62 @@ btnRestart.addEventListener( 'click', () => {
 class game {
 
   constructor() {
+    this.actualLevel = 10
+    this.buttons = {
+      topLeft: document.getElementById( 'num-1' ),
+      topRight: document.getElementById( 'num-2' ),
+      downLeft: document.getElementById( 'num-3' ),
+      downRight: document.getElementById( 'num-4' )
+    }
     this.start()
   }
   
   start() {
     this.createNewSequence = this.createNewSequence.bind(this)
     this.showSequence = this.showSequence.bind(this)
+    this.illuminateButton = this.illuminateButton.bind(this)
+    this.numToString = this.numToString.bind(this)
     this.createNewSequence()
     this.showSequence()
-    // this.desactivateButton()
+
     btnStart.disabled = true
     btnRestart.disabled = false
-  }
 
-  desactivateButton() {
-    if (btnRestart.disabled) {
-      btnRestart.disabled = false
+  }
+  
+  createNewSequence() {
+    this.sequence = new Array(10).fill(0).map( n => Math.floor( Math.random() * 4 ) )
+  }
+  
+  showSequence() {
+    console.log( this.sequence )
+    for( var i = 0; i < this.actualLevel; i++ ) {
+      let piece = this.sequence[i]
+      console.log( piece )
+      setTimeout( () => {
+        this.illuminateButton( piece )
+      }, 1200 * i )
     }
   }
 
-  createNewSequence() {
-    this.sequence = new Array(10).fill(0).map( n => Math.floor(Math.random() * 4) )
+  numToString( n ) {
+    switch( n ) {
+      case 0:
+        return this.buttons.topLeft
+      case 1:
+        return this.buttons.topRight
+      case 2:
+        return this.buttons.downLeft
+      case 3:
+        return this.buttons.downRight
+    }
   }
 
-  level() {
-    this.actualLevel = 0
-  }
-
-  showSequence() {
-    console.log(this.sequence)
+  illuminateButton( n ) {
+    this.numToString( n ).classList.add( 'tablero__pieza-iluminada' )
+    setTimeout( () => {
+      this.numToString( n ).classList.remove( 'tablero__pieza-iluminada' )
+    }, 600 )
   }
 
 }
-
-
-
-
